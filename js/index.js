125894844f86eeec210660af9e865c75f3c7fbdb
qiyunkong <46516985+qiyunkong@users.noreply.github.com>
2020-05-10 16:32:15 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@
         var video = $("#demo");
         video.style="display: block;";
         navigator.mediaDevices.getUserMedia({
+            audio: true,
             //video:true,
             video: { 
                 facingMode: { 
